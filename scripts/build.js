93616328879ddbb62c1e8e57ad478328d8f7907b
chengweiliang <chengweiliang@xin.com>
2018-04-13 12:07:13 +0800

    remove public

--- scripts/build.js
+++ scripts/build.js
@@ -57,7 +57,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
     // if you're in it, you don't end up in Trash
     fs.emptyDirSync(paths.appBuild);
     // Merge with the public folder
-    copyPublicFolder();
+    // copyPublicFolder();
     // Start the webpack build
     return build(previousFileSizes);
   })
@@ -184,9 +184,9 @@ function build(previousFileSizes) {
   });
 }
 
-function copyPublicFolder() {
-  fs.copySync(paths.appPublic, paths.appBuild, {
-    dereference: true,
-    filter: file => file !== paths.appHtml,
-  });
-}
+// function copyPublicFolder() {
+//   fs.copySync(paths.appPublic, paths.appBuild, {
+//     dereference: true,
+//     filter: file => file !== paths.appHtml,
+//   });
+// }
